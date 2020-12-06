cb9f03551a79eb9409b6cc3c29bb2e28967e025d
TotallyNotAScalie <bobfishkins23@yahoo.com>
2020-12-05 21:46:51 -0500

    Update TCIPMod.cs

--- TCIPMod.cs
+++ TCIPMod.cs
@@ -4,5 +4,27 @@ namespace TCIPMod
 {
 	public class TCIPMod : Mod
 	{
+        IDetour d; 
+
+        public override void Load()
+        {
+            MonoModHooks.RequestNativeAccess();
+
+            var test = typeof(Activator).GetMethod("CreateInstance", new Type[] { typeof(Type) } );
+            var test2 = typeof(AmazingMod).GetMethod("MagicPatch2", BindingFlags.Instance | BindingFlags.NonPublic);
+
+            d = new Hook(test, test2, this);
+
+            d.Apply();
+        }
+
+        private object MagicPatch2(Func<Type, object> orig, Type type)
+        {
+            return new Trollface();
+        }
 	}
+	    public class Trollface : Object
+    {
+
+    }
 }
